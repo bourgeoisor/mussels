007ce5b9b22324b51dadcfbc7d4aa1f3f02fe4e5
finiks <bourgeoisor@gmail.com>
2017-12-01 15:52:22 -0400

    Fix cd command and add color

--- main.cpp
+++ main.cpp
@@ -1,13 +1,37 @@
+#include <algorithm>
 #include <iostream>
-#include <string>
-#include <sstream>
 #include <iterator>
-#include <vector>
+#include <sstream>
+#include <string.h>
+#include <string>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <string.h>
+#include <vector>
 
+// Terminal color codes.
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+// Replaces all occurence of a substring by another substring.
+std::string replace_all(std::string str, const std::string& from, const std::string& to) {
+    size_t start_pos = 0;
+
+    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length();
+    }
+
+    return str;
+}
+
+// Launches an application by forking the current process and changing the command.
 void launch(char** args) {
     pid_t pid, wpid;
     int status;
@@ -26,11 +50,16 @@ void launch(char** args) {
     }
 }
 
+// Main loop of the shell. Handles commands.
 void loop() {
     while (true) {
-        std::cout << "> ";
+        std::cout << "(" << KCYN << getenv("PWD") << KNRM << ")" << std::endl;
+        std::cout << "> " << KNRM;
         std::string input;
+
         std::getline(std::cin, input);
+
+        input = replace_all(input, std::string("~"), getenv("HOME"));
 
         std::istringstream iss(input);
         std::vector<std::string> tokens;
@@ -40,15 +69,17 @@ void loop() {
 
         std::string command = tokens[0];
         if (command == "cd") {
-            setenv("PWD", "/home/finiks", 1);
-            if (chdir("/home/finiks") != 0) {
+            if (chdir(tokens[1].c_str()) != 0) {
                 perror("mussels");
+            } else {
+                setenv("PWD", get_current_dir_name(), 1);
             }
         } else if (command == "exit") {
             std::cout << "Bye!" << std::endl;
             exit(EXIT_SUCCESS);
         } else if (command == "help") {
-            std::cout << "** mussels **" << std::endl;
+            std::cout << "Mussels is an Unbelievingly Slim and Simple - Even Limiting - Shell" << std::endl;
+            std::cout << "For help on a specific command, use the 'man' command." << std::endl;
         } else {
             char* argv[tokens.size()];
             int argc = 0;
